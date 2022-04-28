b76ad51015fcd14f3043d0ba854a953aa8e98f7a
Vincent Minde <vincentminde@gmail.com>
2022-04-28 14:32:18 +0300

    Fixed URL

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,13 +24,13 @@ const config = {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-          editUrl: "https://github.com/udsm-dhis2-lab/icare/edit/feature/docs/docs/"
+          editUrl: "https://github.com/udsm-dhis2-lab/icare-docs/edit/master/docs/"
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/main/website/blog/',
+            'https://github.com/udsm-dhis2-lab/icare-docs/edit/master/blog/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
